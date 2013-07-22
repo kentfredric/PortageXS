eecaa448c34b709813e7e692fa59f7a45305ce18
Kent Fredric <kentfredric@gmail.com>
2013-07-23 03:21:36 +1200

    Build results of 2732648 (on maint)

--- lib/PortageXS/Core.pm
+++ lib/PortageXS/Core.pm
@@ -6,7 +6,7 @@ BEGIN {
   $PortageXS::Core::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $PortageXS::Core::VERSION = '0.2.12';
+  $PortageXS::Core::VERSION = '0.2.11';
 }
 
 # ABSTRACT: Core behaviour role for C<PortageXS>
@@ -914,7 +914,7 @@ PortageXS::Core - Core behaviour role for C<PortageXS>
 
 =head1 VERSION
 
-version 0.2.12
+version 0.2.11
 
 =head1 AUTHORS
 
